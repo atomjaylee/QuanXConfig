b3b22fcc5a0bbfa9caf6b105857a951abade26a3
atomjaylee <908105576@qq.com>
2023-01-16 11:32:22 +0800

    feat: version 2

--- scripts/duoduo.js
+++ scripts/duoduo.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2023-01-16 11:20:27
  * @FilePath: /QuanXConfig/scripts/duoduo.js
- * @LastEditTime: 2023-01-16 11:26:04
+ * @LastEditTime: 2023-01-16 11:32:14
  * @Description: 多多视频VIP测试
  */
 
@@ -10,8 +10,8 @@ const url = $request.url;
 let obj = JSON.parse($response.body);
 
 if (obj.data) {
-  obj.data.level = "2";
-  obj.data.valid = true;
+  obj.data.level = "99";
+  obj.data.valid = false;
   obj.data.expiredTime = +new Date("2099/12/31");
 }
 
