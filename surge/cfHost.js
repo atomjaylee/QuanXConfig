2c3e51a6ea58dab33d2dc460ffefccf43ae6071e
atomjaylee <908105576@qq.com>
2023-11-24 10:51:12 +0800

    feat: update

--- surge/cfHost.js
+++ surge/cfHost.js
@@ -1,7 +1,7 @@
 const ssid = $network.wifi.ssid;
 // $notification.post("SSID名称", "", ssid);
 if (ssid == "Kikyo") {
-  $done({ address: "172.66.2.111", ttl: 600 });
+  $done({ address: "104.21.230.67", ttl: 600 });
 } else if (ssid == "atomjaylee" || ssid == "atomjaylee_5G") {
   $done({ address: "104.18.124.62", ttl: 600 });
 } else if (ssid == null) {
