1986a47a7f4844fc8dd7a6fd91b2c7c32cf5e20d
atomjaylee <908105576@qq.com>
2023-12-02 17:30:51 +0800

    feat: cf优选替换

--- surge/cfHost.js
+++ surge/cfHost.js
@@ -3,9 +3,9 @@ const ssid = $network.wifi.ssid;
 if (ssid == "Kikyo") {
   $done({ address: "104.21.230.67", ttl: 600 });
 } else if (ssid == "atomjaylee" || ssid == "atomjaylee_5G") {
-  $done({ address: "104.18.124.62", ttl: 600 });
+  $done({ address: "162.159.43.32", ttl: 600 });
 } else if (ssid == null) {
-  $done({ address: "104.18.124.62", ttl: 600 });
+  $done({ address: "162.159.43.32", ttl: 600 });
 } else {
   $done({});
 }
