b7b059b24c175371d6471009ffa4ac2c4e27eac1
Atom Jaylee <908105576@qq.com>
2025-10-05 02:24:18 +0800

    Update filmix.js

--- rewrite/filmix.js
+++ rewrite/filmix.js
@@ -1,25 +1,36 @@
 var anye = JSON.parse($response.body)
 anye = {
-  "first_vip_start_time" : "2025-04-28T03:46:04Z",
-  "year_total_match" : 2,
-  "hash_version" : 0,
-  "created_at" : "2025-03-08T02:38:54.339Z",
-  "check_appleid_subscription" : false,
-  "userprofile_id" : "_42a87dba768b4b7e7300b54f9e15397a",
-  "vip_start_time" : "2025-04-28T03:46:01Z",
-  "token" : "dc4768a21edba9193f05c9aac06053763c602ae8",
-  "vip_level" : 1,
-  "updated_at" : "2025-06-25T06:49:53.210Z",
-  "app_account_token" : "cffa043b-f905-4f55-89fc-50966a523217",
-  "is_need_refresh" : false,
-  "is_vip" : true,
-  "user" : {
-    "username" : "4a70ea",
-    "id" : 13534,
-    "email" : "",
-    "is_active" : true
-  },
-  "vip_end_time" : "2099-04-28T03:46:01Z"
+    "userprofile_id": "_42a87dba768b4b7e7300b54f9e15397a",
+    "nickname": "Filmixer_4a70ea",
+    "nickname_last_changed_at": null,
+    "is_vip": true,
+    "vip_level": 1,
+    "user": {
+        "id": 13534,
+        "username": "4a70ea",
+        "email": "",
+        "is_active": true
+    },
+    "token": "dc4768a21edba9193f05c9aac06053763c602ae8",
+    "first_vip_start_time": "2025-04-28T03:46:04Z",
+    "vip_start_time": "2025-04-28T03:46:01Z",
+    "vip_end_time": "2099-04-28T03:46:01Z",
+    "year_total_match": 38330,
+    "created_at": "2025-03-08T02:38:54.339Z",
+    "updated_at": "2025-10-02T05:07:01.793Z",
+    "app_account_token": "cffa043b-f905-4f55-89fc-50966a523217",
+    "is_need_refresh": false,
+    "hash_version": 0,
+    "check_appleid_subscription": false,
+    "register_source": "apple",
+    "social_auth": {
+        "provider": "apple",
+        "social_id": "001204.219d742d608549e38c0fea255a481c55.0416",
+        "social_username": "\u674e\u6770",
+        "social_email": "t4fqcrxfvp@privaterelay.appleid.com",
+        "is_email_hidden": true
+    },
+    "source_config_upload_status": "completed"
 }
 
 $done({body : JSON.stringify(anye)})
